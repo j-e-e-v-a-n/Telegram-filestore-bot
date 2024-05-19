af3ab48789673e9204784b48e71f3cf7ddfb3710
Rahul P S <rahulps1000@gmail.com>
2024-05-19 17:50:51 +0530

    added username support for fsub

--- config.py
+++ config.py
@@ -4,6 +4,7 @@
 
 
 import os
+import re
 import logging
 from logging.handlers import RotatingFileHandler
 
@@ -32,7 +33,10 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+fsub = os.environ.get("FORCE_SUB_CHANNEL", "0")
+
+if bool(re.match(r"^[-+]?\d+(\.\d+)?$", fsub)):
+    FORCE_SUB_CHANNEL = int(fsub) 
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
